4ee632dbcb3b966851cceca35d37545097732bad
Louisa May Jasmine G. Miguel <louisa.may05@gmail.com>
2020-03-12 16:06:20 +0800

    Admin interface

--- src/servlets/SettingsServlet.java
+++ src/servlets/SettingsServlet.java
@@ -2,15 +2,12 @@ package servlets;
 
 import java.io.IOException;
 
-import javax.mail.MessagingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import others.Mail;
-import others.RandomString;
 import services.AccountSvc;
 
 public class SettingsServlet extends HttpServlet {
